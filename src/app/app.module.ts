778c992e5faca8988ca9aea9ade255d9cbdbe6a3
LinhChi1502 <linhchidkc@gmail.com>
2020-12-27 19:32:29 +0700

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateProductComponent } from './product/create-product/create-product.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { ListProductComponent } from './product/list-product/list-product.component';
+import { EditProductComponent } from './product/edit-product/edit-product.component';
+import { DeleteProductComponent } from './product/delete-product/delete-product.component';
+import { DetailProductComponent } from './product/detail-product/detail-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProductComponent,
+    ListProductComponent,
+    EditProductComponent,
+    DeleteProductComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
